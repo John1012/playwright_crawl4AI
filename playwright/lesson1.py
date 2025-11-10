15eaad70d2d10b966e8dabf5a63b2d563b115d6f
徐國堂 <roberthsu2003@gmail.com>
2025-11-10 09:34:01 +0800

    modify

--- playwright/lesson1.py
+++ playwright/lesson1.py
@@ -2,9 +2,9 @@ from playwright.sync_api import sync_playwright
 
 def run():
     with sync_playwright() as p:
-        browser = p.webkit.launch(headless=False)
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
-        page.goto("https://www.google.com")
+        page.goto('https://www.google.com')
         print(page.title())
         browser.close()
 
